900524f3402f0c9aae2c3ddd36e49c95a961ad52
Phùng Đức Nhật <ducnhatphung@gmail.com>
2017-10-28 03:56:56 +0700

    wip: authentication

--- store.py
+++ store.py
@@ -1,11 +1,26 @@
 import pandas as pd
 
+
+auth_cache = null
+
+def get_users():
+    if auth_cache == null:
+        # Load username and password from CSV file.
+        df = pd.read_csv(
+            ".//auth.csv",
+            sep="|",        # TODO: Remove separators from username and password.
+            usecols=[0, 1],
+            names=['username', 'password']
+        )
+        auth_cache = df     # Cache username and password.
+    return df
+
+
 def get_films():
-    data = pd.read_csv(
+    df = pd.read_csv(
         ".//foo.csv",
         sep="|",
         usecols=[0, 1, 2, 4, 24],
         names=['id', 'name', 'date', 'link', 'image_url']
         )
-
-    return data.to_json()
+    return df.to_json()
